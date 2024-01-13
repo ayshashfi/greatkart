538e21af6f0ed106c464a0fed96b6811f80dbe45
Ayshath Ashfiya A <ayshathashfiyaa@gmail.com>
2024-01-13 16:59:49 +0530

    postgres db added

--- greatkart/greatkart/settings.py
+++ greatkart/greatkart/settings.py
@@ -112,10 +112,22 @@ AUTH_USER_MODEL = 'accounts.Account'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "greatkart",
+        "USER":"greatkart",
+        "PORT":"5432",
+        "PASSWORD":"greatkart",
+        "HOST":"greatkart.c1kogski4k8x.us-east-1.rds.amazonaws.com",
+        
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
